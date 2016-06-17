b94b78c327b82fb2268080ac0c7d80fe13cb2947
YornChu <chuchuyaoyao@126.com>
2016-06-17 15:59:53 +0800

    Polymorphism:son<-father

--- C.java
+++ C.java
@@ -15,7 +15,7 @@ class B extends A
 	{
 		System.out.println("this is func1 from B");
 	}
-	public void fun3()
+	public void func3()
 	{
 		System.out.println("this is func3 from c");
 	}
@@ -24,15 +24,22 @@ class C
 {
 	public static void main(String [] args)
 	{
-		A a=new A();
-		 B b=a;
+		B b=new B();
+		A a=b;
 		callA(a);
 		callA(new B());
 	}
-	public static void callA(A a)
+	/*public static void callA(A a)
 	{
 		a.func1();
 		a.func2();
+	}*/
+	public static void callA(A a)
+	{
+		B b=(B) a;
+		b.func1();
+		b.func2();
+		b.func3();
 	}
 }
 
