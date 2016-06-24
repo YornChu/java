4269ce9e3aed1b0c2f1bb4d1a5c290a44b844ec9
YornChu <chuchuyaoyao@126.com>
2016-06-24 17:23:58 +0800

    import package

---
+++ TestPackage.java
@@ -0,0 +1,12 @@
+package org.it315;
+import org.it315.example.*;
+public class TestPackage
+{
+	public static void main(String [] args)
+	{
+		//System.out.println("the program is demo");
+		new TestPackage1().print();
+	}
+}
+
+
---
+++ TestPackage1.java
@@ -0,0 +1,8 @@
+package org.it315.example;
+public class TestPackage1
+{
+	public void print()
+	{
+		System.out.println("the program is demo");
+	}
+}
